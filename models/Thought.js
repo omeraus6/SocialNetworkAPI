b2fc2f67625758c4847a715ae7695a3332c57cdc
omeraus6 <omeraus6@yahoo.com>
2024-02-22 01:27:24 -0800

    add starting code

---
+++ models/Thought.js
@@ -0,0 +1,48 @@
+const { Schema, Types } = require('mongoose');
+const { User } = require('./User');
+const {Reaction} = require('./Reaction');
+
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 200,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //get: (date) => formatDate(date)
+
+    },
+    username: {   
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction], // back to it
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+
+//thoughtSchema
+ // .virtual('reactionCount')
+ // .get(function () {
+ //   return this.reactions.length;
+ // });
+
+const Thought = model('thought', thoughtSchema);
+//console.log(formatDate("2022-05-24T01:31:56.774Z"))
+
+module.exports = Thought;
+
+//module.exports = thoughtSchema;
